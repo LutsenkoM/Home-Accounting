41818a4430c863b5cecae839e00c978eab07aa40
Maksym Lutsenko <maksym.m.lutsenko@gmail.com>
2019-05-02 18:48:04 +0300

    init

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to Home-Accounting!');
+    expect(page.getParagraphText()).toEqual('Welcome to home-accounting!');
   });
 });
