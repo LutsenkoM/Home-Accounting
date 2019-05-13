d788679002c6fc85c2a70288ecb24ae5ab9b29f4
Maksym Lutsenko <maksym.m.lutsenko@gmail.com>
2019-05-13 18:48:00 +0300

    Create system page module

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {SharedModule} from "../shared/shared.module";
+import {SystemRoutingModule} from "./system-routing.module";
+import { SystemComponent } from './system.component';
+import { BillPageComponent } from './bill-page/bill-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { PlaningPageComponent } from './planing-page/planing-page.component';
+import { RecordsPageComponent } from './records-page/records-page.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    SystemRoutingModule
   ],
-  declarations: []
+  declarations: [SystemComponent, BillPageComponent, HistoryPageComponent, PlaningPageComponent, RecordsPageComponent]
 })
 export class SystemModule { }
