9b416d12a59f4cad43fc67ce2b51def0d440c0c7
Maksym Lutsenko <maksym.m.lutsenko@gmail.com>
2019-05-22 19:09:37 +0300

    Records page, add events. Part 1

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Category} from "../../shared/models/category.model";
+import {NgForm} from "@angular/forms";
+
+import * as moment from 'moment';
+import {APPEvent} from "../../shared/models/event.model";
 
 @Component({
   selector: 'app-add-event',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEventComponent implements OnInit {
 
+  @Input() categories: Category[] = [];
+  types = [
+    {type: 'income', label: 'Income'},
+    {type: 'outcome', label: 'Outcome'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.categories)
+  }
+
+  onSubmit(form: NgForm) {
+    let {amount, description, category, type} = form.value;
+    if (amount < 0 ) amount *= -1;
+
+    const event = new APPEvent(
+      type, amount, +category, moment().format('DD:MM:YYYY HH:mm:ss'), description
+    );
+
+
   }
 
 }
