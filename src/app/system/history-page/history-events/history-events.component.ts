8c4e09918588b0fc66f6eaedb153fb17d4fffc37
Maksym Lutsenko <maksym.m.lutsenko@gmail.com>
2019-06-04 14:04:02 +0300

    History page. Events list

--- src/app/system/history-page/history-events/history-events.component.ts
+++ src/app/system/history-page/history-events/history-events.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {APPEvent} from "../../shared/models/event.model";
+import {Category} from "../../shared/models/category.model";
 
 @Component({
   selector: 'app-history-events',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryEventsComponent implements OnInit {
 
+  @Input() categories: Category[] = [];
+  @Input() events: APPEvent[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.events.forEach((e)=>{
+      e.catName = this.categories.find( c => c.id === e.category ).name;
+    });
+  }
+
+  getEventClass(e: APPEvent) {
+    return {
+      'label': true,
+      'label-danger': e.type === 'outcome',
+      'label-success': e.type === 'income',
+    }
   }
 
 }
